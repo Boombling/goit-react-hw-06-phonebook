1553cce6ebc066418272a937cf2d346e03846b62
Dmytro Filius <mindertan@gmail.com>
2021-06-21 15:49:22 +0300

    adds a save method to local storage

--- src/App.js
+++ src/App.js
@@ -40,6 +40,20 @@ class App extends Component {
     }))
   }
   
+    componentDidMount() {
+      const contact = localStorage.getItem('contacts');
+      const parsedContacts = JSON.parse(contact);
+
+      if (parsedContacts) {
+      this.setState({contacts: parsedContacts})
+      }
+    }
+
+   componentDidUpdate(prevProps, prevState) {
+     if (this.state.contacts !== prevState.contacts) {
+          localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+        }
+    }
   render() {
     return (
       <div>
